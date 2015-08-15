84e5d6d23f35bca53c5869e28bf553b2e61d9e3c
Cobolock <noonehasacat@gmail.com>
2015-08-15 19:11:14 +0300

    Some testing

--- views.py
+++ views.py
@@ -1,6 +1,4 @@
-from app import app
-from app.response import Response
-from app.controller import Controller
+from infer import app
 from flask import render_template, jsonify, request
 
 
@@ -12,18 +10,4 @@ def index():
 
 @app.route('/r', methods=['POST'])
 def handle_query():
-    response = Response()
-    controller = Controller(response)
-    func_to_call = controller.get_func(request.json['request'])
-
-    try:
-        func_to_call(request.json['attr'])
-
-    except ValueError:
-        controller.log_error("Error: call to non-existent model: ")
-        controller.log_error(request.json['attr'])
-        response.append('e', 'Function was forgotten to be realized: ' +
-                        func_to_call)
-
-    print(response.answers)
-    return jsonify(response.prepare())
+    return 'olo'
